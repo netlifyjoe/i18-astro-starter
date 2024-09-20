b7b1d160ee3281612b484106b0491281224d0d18
Anil Bridgpal <bridgpal@gmail.com>
2024-09-19 20:30:11 -0400

    trying things

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,6 @@ import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-import dotenv from 'dotenv'
-dotenv.config()
-
-const locales = process.env.SUPPORTED_LOCALES.split(',');
-const defaultLocale = process.env.DEFAULT_LOCALE;
-
-function getLocaleFallback() {
-    let fallback = {};
-    locales.forEach((locale) => {
-        if(locale != defaultLocale) {
-            fallback[locale] = process.env.DEFAULT_LOCALE;
-        }
-    });
-
-    return fallback;
-}
-
 export default defineConfig({
     integrations: [
         react(),
@@ -28,15 +11,14 @@ export default defineConfig({
         })
     ],
     output: 'hybrid',
+    site: 'https://i18-astro-starter.netlify.app',
     adapter: netlify(),
     i18n: {
-        defaultLocale: defaultLocale,
-        locales: locales,
-        // routing: "manual"
-        fallback: getLocaleFallback(),
+        defaultLocale: 'en',
+        locales: ['en', 'jp'],
         routing: {
-            fallbackType: "rewrite",
-        }
+            prefixDefaultLocale: false,
+        },
     },
     trailingSlash: 'never'
 });
