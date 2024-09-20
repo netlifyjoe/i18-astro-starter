ea7d5cc450fe27fe2329aba579afe4a91ea3cccd
netlifyjoe <joe.keyes@netlify.com>
2024-09-20 09:25:16 -0400

    Redirect Test 17

--- src/styles/middleware.mjs
+++ src/styles/middleware.mjs
@@ -5,7 +5,6 @@ import { actions } from 'astro:actions';
 export const userMiddleware = defineMiddleware(async (ctx, next) => {
   // this response might come from Astro's i18n middleware, and it might return a 404
   const response = await next();
-  
 
   // the /about page is an exception and we want to render it
   if (ctx.url.pathname.startsWith("/about")) {
