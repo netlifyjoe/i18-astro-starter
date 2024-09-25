92049374b37e7c40f6e85a8342fa22a2ab5cc982
Anil Bridgpal <bridgpal@gmail.com>
2024-09-25 10:26:18 -0400

    updating

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,14 +29,5 @@ export default defineConfig({
     ],
     output: 'hybrid',
     adapter: netlify(),
-    i18n: {
-        defaultLocale: defaultLocale,
-        locales: locales,
-        // routing: "manual"
-        fallback: getLocaleFallback(),
-        routing: {
-            fallbackType: 'rewrite'
-        }
-    },
     trailingSlash: 'ignore'
 });
